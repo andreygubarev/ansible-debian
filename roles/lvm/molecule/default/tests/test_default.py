2adddf71f704e1482e18f9e0fb9d31e1f1c7ec59
Andrey Gubarev <andrey@andreygubarev.com>
2023-06-15 17:17:31 +0400

    created volume test

--- roles/lvm/molecule/default/tests/test_default.py
+++ roles/lvm/molecule/default/tests/test_default.py
@@ -1,10 +1,6 @@
 """Role testing files using testinfra."""
 
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
-
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
+def test_logical_volume(host):
+    """Test if logical volume exists."""
+    assert host.file("/dev/mapper/vg0-lv0").exists
