4154a25ff5f461632476b1eb8e7dc6531286c338
Andrey Gubarev <andrey@andreygubarev.com>
2023-06-15 16:12:32 +0400

    add tests

--- roles/allocate_freespace/molecule/default/tests/test_default.py
+++ roles/allocate_freespace/molecule/default/tests/test_default.py
@@ -1,10 +1,5 @@
 """Role testing files using testinfra."""
 
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
-
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
+def test_freespace(host):
+    assert host.block_device("/dev/vdb2").is_partition
